706663438f5ade174557d08dfe1b7e72ce52e1cc
Pedro Muraki <pedromuraki@gmail.com>
2019-02-17 22:18:42 -0300

    Refactored map function

--- src/map.js
+++ src/map.js
@@ -1,6 +1,4 @@
-// let newArr = [];
-
-const map = (arr, fn, newArr = [], i = 0) => {
+const map = (arr, fn) => {
   if (!Array.isArray(arr)) {
     throw new Error('map should receive an array as first parameter');
   }
@@ -9,10 +7,16 @@ const map = (arr, fn, newArr = [], i = 0) => {
     throw new Error('map should receive a function as second parameter');
   }
 
-  if (arr.length === i) return newArr;
+  const mapInternal = (arrayInternal, i) => {
+    // if (arr.length === i) return [];
+    // return [fn(arr[i], i, arr)].concat(mapInternal(arr, fn, i + 1));
+    const [head, ...tail] = arrayInternal;
+    if (arrayInternal.length === 0) return [];
+    // return [fn(head, i, arr)].concat(mapInternal(tail, i + 1));
+    return [fn(head, i, arr), ...mapInternal(tail, i + 1)];
+  }
 
-  newArr.push(fn(arr[i], i, arr));
-  return map(arr, fn, newArr, i + 1);
+  return mapInternal(arr, 0);
 }
 
 export default map;
